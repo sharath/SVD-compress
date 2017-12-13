684ad7f576cc8831d02884d898245b0e00168e9b
sharath <sharathramku@umass.edu>
2017-12-13 18:54:09 -0500

    rofl i think it works

--- compress.py
+++ compress.py
@@ -1,17 +1,34 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import time
 from PIL import Image
 import sys
-
-def compress(file):
-    im = Image.open(file)
+import os
 
 
+def compress(file, n):
+    img = Image.open(file)
+    imgbw = img.convert('LA')
+
+    #imgbw.convert('RGB').save("bw.jpg")
+
+    # convert to matrix
+    imgmat = np.array(list(imgbw.getdata(band=0)), float)
+    imgmat.shape = (imgbw.size[1], imgbw.size[0])
+    imgmat = np.matrix(imgmat)
+
+    U, sigma, V = np.linalg.svd(imgmat)
+    reconstimg = np.matrix(U[:, :n] * np.diag(sigma[:n]) * np.matrix(V[:n, :]))
+
+    img = Image.fromarray(reconstimg).convert('RGB')
+    if not os.path.exists("processed"):
+        os.mkdir("processed")
+    img.save("processed/processed%d.jpg"%n)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Invalid Parameters")
         exit(0)
-    compress(sys.argv[1])
+    for i in range(0, int(sys.argv[2])+1):
+        compress(sys.argv[1], i)
+
+
