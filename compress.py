fa8ea458b63cb8fb8630abbf37b59803e2800e19
sharath <sharathramku@umass.edu>
2017-12-13 18:24:15 -0500

    added argv

--- compress.py
+++ compress.py
@@ -1,4 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL.Image as img
+import time
+from PIL import Image
+import sys
 
+def compress(file):
+    im = Image.open(file)
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Invalid Parameters")
+        exit(0)
+    compress(sys.argv[1])
