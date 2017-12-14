9ec3b25b1d96a5b140e72849ae2413ab238f5fd0
sharath <sharathramku@umass.edu>
2017-12-13 22:59:45 -0500

    added number for number of eigensvalues from diagonal matrix

--- compress.py
+++ compress.py
@@ -1,5 +1,5 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import sys
 import os
 
@@ -8,8 +8,8 @@ def compress(file, n, s):
     img = Image.open(file)
     imgbw = img.convert('LA')
 
-    if not os.path.exists("processed"):
-        os.mkdir("processed")
+    font = ImageFont.truetype("Prototype.ttf", 30)
+    yellow = 255, 255, 0
     imgbw.convert('RGB').save("processed/bw.png", compress_level=1)
 
     # convert to matrix
@@ -18,11 +18,15 @@ def compress(file, n, s):
     imgmat = np.matrix(imgmat)
 
     U, sigma, V = np.linalg.svd(imgmat)
+
     for i in range(0, n, s):
         if i == 0:
             continue
         reconstimg = np.matrix(U[:, :i] * np.diag(sigma[:i]) * np.matrix(V[:i:]))
         img = Image.fromarray(reconstimg).convert('RGB')
+        draw = ImageDraw.Draw(img)
+        draw.text((10, 10), "%d" % i, yellow, font=font)
+
         img.save("processed/processed%d.png" % i, compress_level=1)
 
 
@@ -30,6 +34,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Invalid Parameters")
         exit(0)
+    if not os.path.exists("processed"):
+        os.mkdir("processed")
     compress(sys.argv[1], int(sys.argv[2])+1, int(sys.argv[3]))
 
 
