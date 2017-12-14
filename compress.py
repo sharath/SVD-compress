4ac368cdcbac512a99dde099d10a56bd1faae127
sharath <sharathramku@umass.edu>
2017-12-14 01:22:13 -0500

    Made more verbose and saves uncompressed file for comparison

--- compress.py
+++ compress.py
@@ -6,45 +6,41 @@ import os
 
 def compress(file, n, s):
     img = np.array(Image.open(file))
+    Image.fromarray(np.uint8(img)).save("processed/unprocessed.png", compress_level=1)
     row, col, _ = img.shape
     img = img / 255
-    img_r = img[:, :, 0]
-    img_g = img[:, :, 1]
-    img_b = img[:, :, 2]
 
     print("Computing SVD for R")
-    U_r, sigma_r, V_r = np.linalg.svd(img_r)
+    U_r, sigma_r, V_r = np.linalg.svd(img[:, :, 0])
     print("Computing SVD for G")
-    U_g, sigma_g, V_g = np.linalg.svd(img_g)
+    U_g, sigma_g, V_g = np.linalg.svd(img[:, :, 1])
     print("Computing SVD for B")
-    U_b, sigma_b, V_b = np.linalg.svd(img_b)
+    U_b, sigma_b, V_b = np.linalg.svd(img[:, :, 2])
 
     font = ImageFont.truetype("Prototype.ttf", 30)
-    yellow = 255, 255, 0
+    font_col = 255, 255, 0
 
     for i in range(0, n, s):
         if i == 0:
             continue
-
         print("Reconstructing R for Term %d" % i)
-        reconst_r = np.dot(U_r[:, :i], np.dot(np.diag(sigma_r[:i]), np.matrix(V_r[:i, :])))
+        reconst_r = np.dot(U_r[:, :i], np.dot(np.diag(sigma_r[:i]), V_r[:i, :]))
         print("Reconstructing G for Term %d" % i)
-        reconst_g = np.dot(U_g[:, :i], np.dot(np.diag(sigma_g[:i]), np.matrix(V_g[:i, :])))
+        reconst_g = np.dot(U_g[:, :i], np.dot(np.diag(sigma_g[:i]), V_g[:i, :]))
         print("Reconstructing B for Term %d" % i)
-        reconst_b = np.dot(U_b[:, :i], np.dot(np.diag(sigma_b[:i]), np.matrix(V_b[:i, :])))
+        reconst_b = np.dot(U_b[:, :i], np.dot(np.diag(sigma_b[:i]), V_b[:i, :]))
 
-        reconstimg = np.zeros((row, col, 3), dtype='uint8')
-        reconstimg[:, :, 0] = reconst_r*255
-        reconstimg[:, :, 1] = reconst_g*255
-        reconstimg[:, :, 2] = reconst_b*255
-
+        reconstimg = np.zeros((row, col, 3))
+        reconstimg[:, :, 0] = reconst_r
+        reconstimg[:, :, 1] = reconst_g
+        reconstimg[:, :, 2] = reconst_b
         reconstimg[reconstimg < 0] = 0
-        reconstimg[reconstimg > 255] = 255
+        reconstimg[reconstimg > 1] = 1
 
-        img = Image.fromarray(reconstimg)
+        print("Converting np Matrix to PIL Image")
+        img = Image.fromarray(np.uint8(reconstimg*255))
         draw = ImageDraw.Draw(img)
-        draw.text((10, 10), "%d" % i, yellow, font=font)
-
+        draw.text((10, 10), "%d" % i, font_col, font=font)
         img.save("processed/processed%d.png" % i, compress_level=1)
 
 
